bb895babc1a2cf2541a5340e1f370afe76eb7b00
Your Name <you@example.com>
2021-06-21 16:11:21 +0500

    behruz

---
+++ dars2.js
@@ -0,0 +1,35 @@
+const pi = 3.14
+// 13
+var R1 = 5
+    R2 = 4
+    S1 = pi*R1
+    S2 = pi*R2
+    S3 = pi*(R1-R2)
+console.log(S1, S2, S3)
+
+var L=4
+    R = 2*(pi*L)
+    S = pi*(Math.pow(R),2)
+    console.log(L, S)
+
+    var L=4
+    S = 2*(pi*L)
+    R = pi*(Math.pow(R),2)
+    console.log(L, S)
+
+    var X1=10
+    X2=27
+    console.log(X2-X1)
+
+    var A=3
+    B=4
+    C=5
+    console.log(A+C,B+C)
+
+    var X2=5
+X1=4
+Y2=6
+Y1=7
+console.log (Math.sqrt (Math.pow(X2-X1)2)+(Math.pow(Y2-Y1)2))
+
+    
---
+++ java.js
@@ -0,0 +1,52 @@
+// begin 1
+
+var a =4
+var b =4
+var P=a+b
+console.log(p=a*b)
+
+
+// begin 2
+var a = 10
+var S =a
+console.log(S=(a*a))
+
+//begin 3
+var a=10
+var b=14
+var S=a+b
+console.log(S=a*b)
+
+var a =20
+var b=7
+var P=a+b
+console.log(p=2*(a+b))
+
+//begin 4
+var pi=3.14
+var d =5
+var L =pi+d
+console.log(L=pi*d)
+
+// begin5
+var a=4
+var V=a
+console.log(V=a*a*a)
+
+var a=6
+var b=12
+var S=a+b
+console.log(S=a*(b*b))
+
+
+//begin 6
+var a=5
+var b=6
+var c=7
+var V=a+b+c
+console.log(V=a*b*c)
+
+var X2=5
+X1=4
+Y2=6
+Y1=7
